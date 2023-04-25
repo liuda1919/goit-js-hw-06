e0e3d40f30242ed640d9a5f3a72c99af0e389287
liuda1919 <121105146+liuda1919@users.noreply.github.com>
2023-04-25 21:27:07 +0300

    update task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,13 +1,10 @@
 const loginForm = document.querySelector('.login-form');
-const rightEmail = 'email@gmail.com';
-const rightPassword = 'password';
 
 loginForm.addEventListener('submit', event => {
     event.preventDefault();
 
     const emailInput = loginForm.elements.email;
     const passwordInput = loginForm.elements.password;
-   if (emailInput.value === rightEmail && passwordInput.value === rightPassword) {
     const newData = {
       elementsEmail: emailInput.value,
       elementsPassword: passwordInput.value
@@ -15,7 +12,7 @@ loginForm.addEventListener('submit', event => {
 
     console.log(newData);
     loginForm.reset();
-  } else if (!emailInput.value || !passwordInput.value) {
+   if (!emailInput.value || !passwordInput.value) {
     alert('Fill up all fields!');
   }
 });
