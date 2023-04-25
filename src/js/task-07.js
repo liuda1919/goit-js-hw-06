b26f07c7d525a84d5bf059da596b66c3b5740412
liuda1919 <121105146+liuda1919@users.noreply.github.com>
2023-04-25 12:47:55 +0300

    update

--- src/js/task-07.js
+++
@@ -1,7 +0,0 @@
-const fontSizeControl = document.querySelector('#font-size-control');
-const input = document.querySelector('#text');
-
-fontSizeControl.addEventListener('input', event => {
-    const fontSize = event.target.value;
-    text.style.fontSize = `${fontSize}px`;
-})  
